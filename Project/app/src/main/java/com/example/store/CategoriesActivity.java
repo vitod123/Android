73fac58756d304ad2cb5a09e89ebc9d36c8abb1b
Vitalii <vitalijmarchuk2508@gmail.com>
2023-10-27 22:17:42 +0200

    d

---
+++ Project/app/src/main/java/com/example/store/CategoriesActivity.java
@@ -0,0 +1,60 @@
+package com.example.store;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.store.category.CategoriesAdapter;
+import com.example.store.category.CategoryCreateActivity;
+import com.example.store.dto.category.CategoryItemDTO;
+import com.example.store.services.ApplicationNetwork;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    RecyclerView rcCategories;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+        rcCategories = findViewById(R.id.rcCategories);
+        rcCategories.setHasFixedSize(true);
+        rcCategories.setLayoutManager(new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false));
+
+        ApplicationNetwork
+                .getInstance()
+                .getCategoriesApi()
+                .list()
+                .enqueue(new Callback<List<CategoryItemDTO>>() {
+                    @Override
+                    public void onResponse(Call<List<CategoryItemDTO>> call, Response<List<CategoryItemDTO>> response) {
+                        if(response.isSuccessful()) {
+                            List<CategoryItemDTO> data = response.body();
+                            CategoriesAdapter adapter = new CategoriesAdapter(data);
+                            rcCategories.setAdapter(adapter);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CategoryItemDTO>> call, Throwable t) {
+
+                    }
+                });
+
+    }
+
+    public void onClickGoToCreateCategory(View view) {
+        Intent intent = new Intent(CategoriesActivity.this, CategoryCreateActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
