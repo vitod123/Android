75bb4d2fb14ee8f59fd26eda98bdc677ac98f332
Vitalii <vitalijmarchuk2508@gmail.com>
2023-11-04 12:42:50 +0100

    comit

---
+++ WebKovbasa/WebKovbasa/Models/Account/RegisterUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebKovbasa.Models.Account
+{
+    public class RegisterUserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
