0ef6a4f6c5028e5aecb9a68df997487ae6f7003b
Vitalii <vitalijmarchuk2508@gmail.com>
2023-11-10 21:10:09 +0100

    last

--- WebKovbasa/WebKovbasa/Controllers/AccountController.cs
+++ WebKovbasa/WebKovbasa/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebKovbasa.Constants;
 using WebKovbasa.Data.Entities.Identity;
+using WebKovbasa.Helpers;
+using WebKovbasa.Interfaces;
 using WebKovbasa.Models.Account;
 
 namespace WebKovbasa.Controllers
@@ -12,19 +14,48 @@ namespace WebKovbasa.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<UserEntity> _userManager;
-        public AccountController(UserManager<UserEntity> userManager)
+        private readonly IJwtTokenService _jwtTokenService;
+        public AccountController(UserManager<UserEntity> userManager,
+            IJwtTokenService jwtTokenService)
         {
             _userManager = userManager;
+            _jwtTokenService = jwtTokenService;
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                bool isPasswordValid = await _userManager.CheckPasswordAsync(user, model.Password);
+                if (!isPasswordValid)
+                {
+                    return BadRequest();
+                }
+                var token = await _jwtTokenService.CreateToken(user);
+                return Ok(new { token });
+            }
+            return BadRequest();
+        }
+
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model)
         {
+            string imageName = null;
+
+            if (!string.IsNullOrEmpty(model.ImageBase64))
+            {
+                imageName = ImageWorker.SaveImage(model.ImageBase64);
+            }
+
             UserEntity user = new UserEntity()
             {
                 FirstName = model.FirstName,
                 LastName = model.LastName,
                 UserName = model.Email,
-                Email = model.Email
+                Email = model.Email,
+                Image = imageName,
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
--- WebKovbasa/WebKovbasa/Migrations/20231108161940_Init Database.Designer.cs
+++ WebKovbasa/WebKovbasa/Migrations/20231108161940_Init Database.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebKovbasa.Data;
@@ -11,9 +12,11 @@ using WebKovbasa.Data;
 namespace WebKovbasa.Migrations
 {
     [DbContext(typeof(AppEFContext))]
-    partial class AppEFContextModelSnapshot : ModelSnapshot
+    [Migration("20231108161940_Init Database")]
+    partial class InitDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
